8654413cb3248d5d7420b1fd28f79b1dc7b05c1f
Rohit <rohit2007rajwar@gmail.com>
2026-08-23 15:13:54 +0530

    Add files via upload
    pratice question 5- the top 3 highest-paid employees IN ASCENDING

---
+++ question 5.sql
@@ -0,0 +1,37 @@
+Use learner;
+Create table employee(Emp_id int NOt null  ,
+Firste_name varchar(50), 
+Department varchar(50),
+ Salary int Not null,
+ HireDate Date
+ );
+Insert into employee values(101 ,"AliceSmith","HR",55000,"2020-03-11"),
+ (102,"BobJohnson","IT",85000,"2019-11-01"),
+ (103,"CharlieBrown","finance",75000,"2021-06-20"),
+ (104,"Brown","finance",79000,"2019-06-28"),(105,"Arown","HR",80000,"2021-08-20"),
+ (106,"DavidMiller","IT", 90000,"2018-05-12");
+
+alter table employee
+add holiday varchar(50);
+select department
+from employee
+where department <>'HR';
+
+select Salary
+from employeeFROM employee 
+WHERE Salary < 60000 OR Salary > 75000;
+
+
+/*Display employees hired before 2020.*/
+SELECT * 
+FROM employee
+WHERE hiredate < '2020-01-01';
+
+/*Display the top 3 highest-paid employees IN ASCENDING .*/
+select max(Salary)
+from employee
+group by First_name
+having max(Salary)
+Order by max(salary)asc
+limit 3;
+
